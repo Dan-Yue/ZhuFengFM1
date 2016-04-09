f2bdeb779ca7aadd5e0736da189a70797b881538
Administrator <1543880711@qq.com>
2016-04-09 21:28:39 +0800

    架构完善,点击发现->推荐界面每一单元格播放相应的声音(测试通过)(添加版权信息)

---
+++ app/src/main/java/com/cain/zhufengfm1/fragment/custom/CustomHistoryFragment.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2016. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+ * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
+ * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
+ * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
+ * Vestibulum commodo. Ut rhoncus gravida arcu.
+ */
+
+package com.cain.zhufengfm1.fragment.custom;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.cain.zhufengfm1.R;
+import com.cain.zhufengfm1.fragment.BaseFragment;
+import com.cain.zhufengfm1.task.TaskCallback;
+import com.cain.zhufengfm1.task.TaskResult;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class CustomHistoryFragment extends BaseFragment implements TaskCallback{
+
+
+    public CustomHistoryFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_custom_history, container, false);
+    }
+
+    @Override
+    public String getFragmentTitle() {
+        return "历史";
+    }
+
+    @Override
+    public void onTaskFinished(TaskResult result) {
+
+    }
+}
